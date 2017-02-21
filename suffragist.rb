5c80b8da99a17b3f78248cc7e7b2902d5fd63ea0
Zalora <Zalora@ZSEALT2014103.local>
2017-02-21 13:11:14 +0800

    Storing data

--- suffragist.rb
+++ suffragist.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'yaml/store'
 
 Choices = {
   'HAM' => 'Hamburger',
@@ -15,10 +16,17 @@ end
 post '/cast' do
   @title = 'Thanks for casting your vote!'
   @vote  = params['vote']
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes'][@vote] ||= 0
+    @store['votes'][@vote] += 1
+  end
   erb :cast
 end
 
 get '/results' do
-  @votes = { 'HAM' => 7, 'PIZ' => 5, 'CUR' => 3 }
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
   erb :results
 end
